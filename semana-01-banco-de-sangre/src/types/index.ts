68e30c44049faa08ccc3605db4dd571d24e19035
alucard592 <kevinbueno081@gmail.com>
2026-09-24 21:21:29 -0500

    refactor: estructurar proyecto de la semana 4 en la raiz

--- semana-01-banco-de-sangre/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface Donante {
-  id: string;
-  nombre: string;
-  tipoSangre: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
-  ultimaDonacion: string; // fecha
-  fotoUrl: string;
-  disponible: boolean;
-}
