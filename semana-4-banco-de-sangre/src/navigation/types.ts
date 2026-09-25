f85a36edbcbf9d6589892d72aa62d150dd18faf3
alucard592 <kevinbueno081@gmail.com>
2026-09-24 21:21:15 -0500

    refactor: estructurar proyecto de la semana 1 en la raiz

--- semana-4-banco-de-sangre/src/navigation/types.ts
+++
@@ -1,9 +0,0 @@
-export type HomeStackParamList = {
-  HomeList: undefined;
-  HomeDetail: { id: string; nombre: string };
-};
-
-export type RootTabParamList = {
-  Home: undefined;
-  Saved: undefined;
-};
